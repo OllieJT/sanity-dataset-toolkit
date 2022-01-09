fdb5b03c5573d5008fbdef513dddc45dd151c5f3
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2022-01-09 12:20:23 +0000

    Update example use

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
-// import { useSanityDataset } from "./lib/sanity/use-dataset";
-import { useExportPath } from "./lib/util/use-path";
+import { useSanityDataset } from "./lib/sanity/use-dataset";
+import { useExportPath, useImportPath } from "./lib/util/use-path";
 import { clearNDJSON } from "./lib/ndjson";
 import { addToDataset } from "./lib/sanity";
 
-// const { importFile } = useImportPath();
+const { importFile } = useImportPath();
 const { exportFile } = useExportPath();
 
+let my_transformed_data: SanityDocument[] = [];
+
+// Clear the export directory
 clearNDJSON({
 	location: exportFile,
 });
 
-/* async function handleImport() {
-	await useSanityDataset({
-		groupByType: true,
-		importFile,
-	});
-} */
-async function handleExport() {
-	console.log("Running");
-	await addToDataset({
-		documents: [{ test: "1" }, { test: "2" }, { test: "3" }, { test: "4" }],
-		exportFile,
-	});
+// Import NDJSON or whatever data you have
+async function handleImport() {
+	const data = await useSanityDataset({ importFile });
+
+	console.log(data);
+	// Do something with your data
+	// my_transformed_data = data.all.map(document => {...})
 }
 
+// Export your transformed data back to NDJSON
+async function handleExport() {
+	const documents = await my_transformed_data;
+	await addToDataset({
+		documents,
+		exportFile,
+	});
+	console.log(`${documents.length} documents added to ${exportFile}`);
+}
+
+handleImport();
 handleExport();
